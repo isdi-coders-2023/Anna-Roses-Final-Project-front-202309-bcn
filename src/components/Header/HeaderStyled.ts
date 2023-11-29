175e60d711fea80dd6e9403fdbc2a838eb978b1a
Anna Roses <anna.roses.marti@gmail.com>
2023-11-29 14:35:01 +0100

    Add Header styles

---
+++ src/components/Header/HeaderStyled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  background-color: ${({ theme }) => theme.color.headerColor};
+  padding: 23px 20px 9px 20px;
+  height: 95px;
+
+  .image {
+    object-fit: contain;
+    height: 63px;
+    width: 280px;
+  }
+`;
+
+export default HeaderStyled;
