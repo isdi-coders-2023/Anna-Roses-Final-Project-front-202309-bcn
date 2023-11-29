fc75c7138e442b0f3b346a4e6f59a3ea658dc008
Anna Roses <anna.roses.marti@gmail.com>
2023-11-29 14:31:48 +0100

    Add Header component and logo

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import HeaderStyled from "./HeaderStyled";
+
+const Header = (): React.ReactElement => {
+  return (
+    <HeaderStyled>
+      <img
+        src="./images/logo.webp"
+        alt="Mi comunidad favorita"
+        className="image"
+        height={63}
+        width={280}
+      />
+    </HeaderStyled>
+  );
+};
+
+export default Header;
