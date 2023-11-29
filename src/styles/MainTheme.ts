8c31a3013daf6c4f58ac45a4773edcc773edda29
Anna Roses <anna.roses.marti@gmail.com>
2023-11-29 14:10:33 +0100

    Add mainTheme and types

---
+++ src/styles/MainTheme.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  color: {
+    mainFontColor: "#000",
+    headerColor: "#0a75bc",
+    errorColor: "#c50000",
+    successColor: "#23632e",
+    inhabilButton: "949699",
+  },
+  typography: {
+    mainFontFamily: "Verdana, Geneva, Tahoma, sans-serif",
+    pageTitleSize: "1.156rem",
+    cardTitleSize: "1.375rem",
+    textSize: "1.125rem",
+    errorPage404: "2.8rem",
+    errorPageOops: "1.8rem",
+  },
+};
+
+export default mainTheme;
