522af2dd4a61da28d068ebcfe7217405fa17652b
Anna Roses <anna.roses.marti@gmail.com>
2023-11-29 14:11:15 +0100

    Add resets and global styles

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*,
+::before,
+::after {
+  box-sizing: border-box;
+}
+
+body,
+h1,
+h2 {
+  margin: 0;
+}
+
+body {
+  font-family: ${({ theme }) => theme.typography.mainFontFamily};
+  color: ${({ theme }) => theme.color.mainFontColor};
+  font-size: ${({ theme }) => theme.typography.textSize};
+}
+
+ul, li {
+  list-style: none;
+  margin-top: 0;
+  padding: 0;
+}
+
+img {
+  width: 100%;
+}
+
+a {
+  text-decoration: none;
+  background-color: transparent;
+  font: inherit;
+  cursor: pointer;
+
+}
+`;
+
+export default GlobalStyle;
