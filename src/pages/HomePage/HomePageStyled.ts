14d0f63082e35266360d5cf497db05a021c12a47
Anna Roses <anna.roses.marti@gmail.com>
2023-11-30 09:53:37 +0100

    Add margin top to title

--- src/pages/HomePage/HomePageStyled.ts
+++ src/pages/HomePage/HomePageStyled.ts
@@ -4,10 +4,12 @@ const HomePageStyled = styled.div`
   display: flex;
   flex-direction: column;
   padding: 10;
+  margin-top: 95px;
 
   .title {
     font-size: ${({ theme }) => theme.typography.pageTitleSize};
     text-align: center;
+    padding: 33px 26px 0 26px;
   }
 `;
 
