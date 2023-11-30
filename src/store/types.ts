7c3890f5ad4035b861c1ee9a827fa2f36724ce46
Anna Roses <anna.roses.marti@gmail.com>
2023-11-30 13:03:12 +0100

    Add neighboursSlice loadNeighbours

---
+++ src/store/types.ts
@@ -0,0 +1,22 @@
+export interface NeighbourStructure {
+  _id: string;
+  name: string;
+  floor: string;
+  door: string;
+  coefficient: number;
+  moneyInFavour: number;
+  ownerFirst: string;
+  ownerSecond: string;
+  powers: string;
+  activityKind: string;
+  isFirstResidence: boolean;
+  isRented: boolean;
+  numberOfResidents: number;
+  yearOfPurchase: number;
+  coments: string;
+  image: string;
+}
+
+export interface NeighboursStructure {
+  neighbours: NeighbourStructure[];
+}
