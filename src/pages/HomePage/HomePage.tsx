edd608678c877baf7c04656cdd321ef7fc9a3cc5
Anna Roses <anna.roses.marti@gmail.com>
2023-12-01 12:20:01 +0100

    Add provisional NeighboursData

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,12 @@
 import HomePageStyled from "./HomePageStyled";
-import mockNeighbours from "../../testUtils/mockNeighbours";
 import { loadNeighboursActioncreator } from "../../store/features/neighbours/neighboursSlice";
 import List from "../../components/List/List";
 import { useAppDispatch } from "../../store/hooks";
+import mockData from "../../Data/NeighboursData/NeighboursData";
 
 const HomePage = (): React.ReactElement => {
   const dispatch = useAppDispatch();
-  const currentNeighbours = mockNeighbours;
+  const currentNeighbours = mockData;
   dispatch(loadNeighboursActioncreator(currentNeighbours));
 
   return (
