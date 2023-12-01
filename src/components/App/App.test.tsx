5b082de15f0188c89f9edad5b8ea48d8755c2a63
Anna Roses <anna.roses.marti@gmail.com>
2023-12-01 18:53:02 +0100

    Move route test from NavMenu to App

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,19 +1,32 @@
 import { screen } from "@testing-library/react";
 import customRender from "../../testUtils/customRender";
 import App from "./App";
-import mockNeighbours from "../../testUtils/mockNeighbours";
+import mockNeighbours from "../../mocks/mockNeighbours";
 
 describe("Given a App component", () => {
+  const mockData = mockNeighbours;
   describe("When it is rendered", () => {
     test(
       "Then it should show an image with an alt text 'Mi comunidad favorita logo'",
     );
     const expectedAltText = "Mi comunidad favorita logo";
-    const mockData = mockNeighbours;
-
     customRender(<App />, mockData);
     const altText = screen.getByRole("img", { name: expectedAltText });
 
     expect(altText).toBeInTheDocument();
+  });
+
+  describe("When it receives a click on the 'Home' link", () => {
+    test("Then it should show a title 'Administra tu comunidad' on a heading", () => {
+      const homePageTitle = "Administra tu comunidad";
+      const homeLink = "Home";
+
+      customRender(<App />, mockData);
+      const link = screen.getByRole("link", { name: homeLink });
+      const title = screen.getByRole("heading", { name: homePageTitle });
+
+      expect(title).toBeInTheDocument();
+      expect(link).toBeInTheDocument();
+    });
   });
 });
