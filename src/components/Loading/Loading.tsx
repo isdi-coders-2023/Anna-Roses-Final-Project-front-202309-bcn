cef34e64da10eed14be532914f905f48f799289a
Anna Roses <anna.roses.marti@gmail.com>
2023-12-03 13:40:36 +0100

    Add Loading component structure

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,18 @@
+import LoadingStyled from "./LoadingStyled";
+
+const Loading = (): React.ReactElement => {
+  return (
+    <LoadingStyled className="loading">
+      <img
+        src="./images/loading.webp"
+        alt=""
+        className="loading__image"
+        width="250"
+        height="250"
+      />
+      <span className="loading__text">Cargando...</span>
+    </LoadingStyled>
+  );
+};
+
+export default Loading;
