f23e5086a423cef4d4aa618b7f4a5910a2d8a2ae
Anna Roses <anna.roses.marti@gmail.com>
2023-12-04 14:39:38 +0100

    Add NotFoundPage testto find the image alt text

---
+++ src/pages/NotFoundPage/NotFoundPage.test.tsx
@@ -0,0 +1,19 @@
+import { screen } from "@testing-library/react";
+import mockNeighbours from "../../mocks/mockNeighbours";
+import { customRender } from "../../testUtils/customRender";
+import NotFoundPage from "./NotFoundPage";
+
+describe("Given a NotFoundPage component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a 'minion in alarm mode'"),
+      () => {
+        const expectedAltText = "minion in alarm mode";
+        const mockList = mockNeighbours;
+
+        customRender(<NotFoundPage />, mockList);
+        const pageNotFoundImage = screen.getByAltText(expectedAltText);
+
+        expect(pageNotFoundImage).toBeInTheDocument();
+      };
+  });
+});
