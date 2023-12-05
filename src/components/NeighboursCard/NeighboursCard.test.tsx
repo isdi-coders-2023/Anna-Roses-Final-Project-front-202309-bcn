9fde448a349e28170bc1019c0d348e7ea6536e2f
Anna Roses <anna.roses.marti@gmail.com>
2023-12-05 13:35:11 +0100

    Add delete Button NeighboursCard test

--- src/components/NeighboursCard/NeighboursCard.test.tsx
+++ src/components/NeighboursCard/NeighboursCard.test.tsx
@@ -1,12 +1,14 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import mockNeighbours from "../../mocks/mockNeighbours";
 import NeighboursCard from "./NeighboursCard";
 import { customRender } from "../../testUtils/customRender";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a Card component", () => {
+  const mockList = mockNeighbours;
+
   describe("When it is rendered and recives a neighbour and a key number 0", () => {
     test("Then it should show 'Marta Ibarra Chef' in the alternative text of the image", () => {
-      const mockList = mockNeighbours;
       const mockNeighbour = mockList[0];
       const expectedText = mockNeighbour.name;
 
@@ -17,6 +19,22 @@ describe("Given a Card component", () => {
       const text = screen.getByRole("img", { name: expectedText });
 
       expect(text).toBeInTheDocument();
+    });
+  });
+
+  describe("When it receives a click on the 'Eliminar 'button of 'Marta Ibarra Chef'", () => {
+    test("Then it should not show 'Marta Ibarra Chef' neighbour", async () => {
+      const buttonText = "Eliminar";
+      const neighbourName = "Marta Ibarra Chef";
+
+      customRender(<NeighboursCard neighbour={mockList[0]} />, mockList);
+      const button = screen.getByRole("button", { name: buttonText });
+      const heading = screen.getByRole("heading", { name: neighbourName });
+      await userEvent.click(button);
+
+      waitFor(() => {
+        expect(heading).not.toBeInTheDocument();
+      });
     });
   });
 });
