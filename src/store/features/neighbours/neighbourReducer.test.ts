7a61de1bfd82a6e37d844b78fc97dffc869c7f59
Anna Roses <anna.roses.marti@gmail.com>
2023-12-05 13:01:38 +0100

    Add deleteNeighbour reducer test

--- src/store/features/neighbours/neighbourReducer.test.ts
+++ src/store/features/neighbours/neighbourReducer.test.ts
@@ -1,6 +1,7 @@
 import mockNeighbours from "../../../mocks/mockNeighbours";
 import { NeighboursStateStructure } from "../types";
 import {
+  deleteNeighbourActionCreator,
   loadNeighboursActionCreator,
   neighboursReducer,
 } from "./neighboursSlice";
@@ -19,6 +20,25 @@ describe("Given a peopleReducer reducer", () => {
       );
 
       expect(newNeighboursState.neighbours).toStrictEqual(neighboursList);
+    });
+  });
+
+  describe("When it receives a list of two neighbours, an existing neighbour id and the deleteNeighbour method", () => {
+    test("Then it should return the list of one neighbour without the deleted neighbour 'Marta Ibarra Chef'", () => {
+      const initialState: NeighboursStateStructure = {
+        neighbours: mockNeighbours,
+      };
+      const expectedDeletedNeighbour = "Marta Ibarra Chef";
+      const expectedNeighbourId = "6563639cc4ddfcae99eeb07a";
+
+      const currentNeighbourState = neighboursReducer(
+        initialState,
+        deleteNeighbourActionCreator(expectedNeighbourId),
+      );
+
+      currentNeighbourState.neighbours.forEach((neighbour) => {
+        expect(neighbour).not.toHaveProperty("name", expectedDeletedNeighbour);
+      });
     });
   });
 });
