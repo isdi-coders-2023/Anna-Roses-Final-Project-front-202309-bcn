20c05a9d8e3a7ec18da55b87a9c8a4046ac0f48a
Anna Roses <anna.roses.marti@gmail.com>
2023-12-07 13:50:09 +0100

    Add shared styled components Page and Title

---
+++ src/styles/shared/PageStyled/PageStyled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const PageStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 95px;
+  margin-bottom: 34px;
+`;
+
+export default PageStyled;
