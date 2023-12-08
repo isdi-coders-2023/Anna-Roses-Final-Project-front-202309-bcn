58210fbf22d47d034555d5ea0a542dbc4396f656
Anna Roses <anna.roses.marti@gmail.com>
2023-12-08 11:47:22 +0100

    Fix form input container size

--- src/components/NeighboursForm/NeighboursFormStyled.ts
+++ src/components/NeighboursForm/NeighboursFormStyled.ts
@@ -2,21 +2,31 @@ import styled from "styled-components";
 
 const NeighboursFormStyled = styled.form`
   display: flex;
-  flex-direction: column;
-  margin-bottom: 30px;
+  justify-content: center;
+  align-items: center;
 
   .form {
-    &__label {
-      font-weight: bold;
-    }
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 30px;
 
     &__input {
       margin: 10px 0 20px 0;
       padding: 6px;
+      font-size: 16px;
+      height: 40px;
     }
 
     &__button {
       text-align: center;
+    }
+  }
+
+  @media (min-width: 650px) {
+    .form {
+      &__input {
+        width: 630px;
+      }
     }
   }
 `;
