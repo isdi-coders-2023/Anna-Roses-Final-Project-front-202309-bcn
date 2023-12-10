bdf73b950d367b4073eca33300c53fb60ee3065f
Anna Roses <anna.roses.marti@gmail.com>
2023-12-10 13:14:13 +0100

    Move test from App to addNeighbourToApi

--- src/hooks/__tests__/addNeighbourToApi.test.tsx
+++ src/hooks/__tests__/addNeighbourToApi.test.tsx
@@ -1,24 +1,49 @@
-import { renderHook } from "@testing-library/react";
-import { mockNeighbours } from "../../mocks/mockNeighbours";
+import { renderHook, screen } from "@testing-library/react";
+import { mockAnaNeighbour, mockNeighbours } from "../../mocks/mockNeighbours";
 import useNeighboursApi from "../useNeighboursApi";
 import { providerWrapper } from "../../testUtils/customWrapper";
+import { errorHandlers } from "../../mocks/handlers";
+import { server } from "../../mocks/node";
+import { customRenderWithoutRouter } from "../../testUtils/customRender";
+import { MemoryRouter } from "react-router-dom";
+import App from "../../components/App/App";
 
 describe("Given a useNeighboursApi custom hook", () => {
+  const neighboursList = mockNeighbours;
+
+  const {
+    result: {
+      current: { addNeighbourToApi },
+    },
+  } = renderHook(() => useNeighboursApi(), { wrapper: providerWrapper });
+
   describe("When it calls its addNewNeighbourToApi method with 'Ana Lambea Andorrana'", () => {
     test("Then it should return 'Ana Lambea Andorrana' from the API", async () => {
-      const neighboursList = mockNeighbours;
-
-      const {
-        result: {
-          current: { addNeighbourToApi },
-        },
-      } = renderHook(() => useNeighboursApi(), { wrapper: providerWrapper });
-
       const newNeighbour = neighboursList[2];
 
       const response = await addNeighbourToApi(newNeighbour);
 
       expect(response).toStrictEqual(newNeighbour);
+    });
+  });
+
+  describe("When it is called with its addNeighbourToApi function with 'Ana Lambea Andorrana' and the response fails", () => {
+    test("Then it should show the text 'No hemos podido añadir el nuevo vecino, inténtalo de nuevo'", async () => {
+      server.use(...errorHandlers);
+      const path = "/create";
+      const errorMessage =
+        "No hemos podido añadir al nuevo vecino, vuelve a intentarlo";
+
+      customRenderWithoutRouter(
+        <MemoryRouter initialEntries={[path]}>
+          <App />
+        </MemoryRouter>,
+      );
+
+      await addNeighbourToApi(mockAnaNeighbour);
+      const feedback = await screen.findByText(errorMessage);
+
+      expect(feedback).toBeInTheDocument();
     });
   });
 });
